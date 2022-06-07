e13278c02239f3ceb7257288ea3ae8d1bc588abc
LucioGhedo <lucioghedina@gmail.com>
2022-06-07 14:38:27 +0200

    first commit

---
+++ milestone-1/js/script.js
@@ -0,0 +1,14 @@
+for(let i = 0; i <= 100; i++) {
+    let fizzBuzz;
+    if(i % 3 === 0 && i % 5 === 0) {
+        fizzBuzz = 'FizzBuzz';
+    } else if (i % 5 === 0) {
+        fizzBuzz = 'Buzz';
+    } else if (i % 3 === 0) {
+        fizzBuzz = 'Fizz';
+    } else {
+        fizzBuzz = i;
+    }
+
+    console.log(fizzBuzz);
+}
